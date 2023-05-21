68c34df89f71d7eb75601e09e82a8f48d85598ae
Souraj Sadhukhan <sou93raj@gmail.com>
2023-05-21 14:56:41 +0530

    few changes

--- header.js
+++ header.js
@@ -7,7 +7,7 @@ class Header extends HTMLElement {
     this.innerHTML = `
     <header class="header site-header main-header    is-not-stuck lqd-smart-sticky-header" data-sticky-header="true"
       data-sticky-values-measured="false" data-sticky-options="{&quot;smartSticky&quot;:true}" id="header"
-      itemscope="itemscope" itemtype="https://schema.org/WPHeader">
+      itemscope="itemscope" itemtype="https://schema.org/WPHeader" style="padding-top: 15px;">
       <style id="elementor-post-7919">
         .elementor-7919 .elementor-element.elementor-element-208375d>.elementor-container>.elementor-column>.elementor-widget-wrap {
           align-content: center;
@@ -624,31 +624,6 @@ class Header extends HTMLElement {
                                     data-id="203a012e" data-element_type="column">
                                     <div class="elementor-widget-wrap elementor-element-populated">
                                       <div
-                                        class="elementor-element elementor-element-73f51f2f elementor-widget elementor-widget-ld_custom_menu"
-                                        data-id="73f51f2f" data-element_type="widget"
-                                        data-widget_type="ld_custom_menu.default">
-                                        <div class="elementor-widget-container">
-                                          <div class="lqd-fancy-menu lqd-custom-menu pos-rel lqd-menu-td-none">
-                                            <ul class="reset-ul inline-nav" id="73f51f2f">
-                                              <li>
-                                                <a href="#">
-                                                  <span
-                                                    class="link-icon d-inline-flex hide-if-empty align-items-center left-icon icon-next-to-label"></span>
-                                                  English
-                                                </a>
-                                              </li>
-                                              <li>
-                                                <a href="#">
-                                                  <span
-                                                    class="link-icon d-inline-flex hide-if-empty align-items-center left-icon icon-next-to-label"></span>
-                                                  Espanol
-                                                </a>
-                                              </li>
-                                            </ul>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div
                                         class="elementor-element elementor-element-6fd6d737 elementor-widget elementor-widget-spacer"
                                         data-id="6fd6d737" data-element_type="widget" data-widget_type="spacer.default">
                                         <div class="elementor-widget-container">
@@ -823,21 +798,10 @@ class Header extends HTMLElement {
                                               data-text-rotator="true">
                                               Collaboration, work
                                               enquires or just say <span class="txt-rotate-keywords"><span
-                                                  class="txt-rotate-keyword active"><span>hello.</span></span><span
-                                                  class="txt-rotate-keyword"><span>你好</span></span><span
-                                                  class="txt-rotate-keyword"><span>もしもし.</span></span><span
-                                                  class="txt-rotate-keyword"><span>hola.</span></span></span>
+                                              class="txt-rotate-keyword active"><span>hello.</span></span><span
+                                              class="txt-rotate-keyword"><span>নমস্কার</span></span><span
+                                              class="txt-rotate-keyword"><span>नमस्ते.</span></span>
                                             </h2>
-                                          </div>
-                                        </div>
-                                      </div>
-                                      <div
-                                        class="elementor-element elementor-element-3f5df9d5 elementor-widget elementor-widget-spacer"
-                                        data-id="3f5df9d5" data-element_type="widget" data-widget_type="spacer.default">
-                                        <div class="elementor-widget-container">
-                                          <div class="elementor-spacer">
-                                            <div class="elementor-spacer-inner">
-                                            </div>
                                           </div>
                                         </div>
                                       </div>
@@ -849,7 +813,7 @@ class Header extends HTMLElement {
                                           <div id="ld-fancy-heading-634fd7e7f33c0" class="ld-fancy-heading pos-rel">
                                             <p
                                               class="ld-fh-element d-inline-block pos-rel normal lqd-highlight-classic lqd-highlight-grow-left p">
-                                              London</p>
+                                              Kolkata</p>
                                           </div>
                                         </div>
                                       </div>
@@ -861,10 +825,8 @@ class Header extends HTMLElement {
                                           <div id="ld-fancy-heading-634fd7e80063b" class="ld-fancy-heading pos-rel">
                                             <p
                                               class="ld-fh-element d-inline-block pos-rel normal lqd-highlight-classic lqd-highlight-grow-left p">
-                                              +) 22 256 7890<br>
-                                              <a href="https://interiortwoarc.liquid-themes.com/cdn-cgi/l/email-protection"
-                                                class="__cf_email__"
-                                                data-cfemail="c8a9baaba0a1bcadabbcbdbaad88a0bdaae6aba7a5">[email&#160;protected]</a>
+                                              8240266320<br>
+                                              8250426185
                                             </p>
                                           </div>
                                         </div>
@@ -899,8 +861,8 @@ class Header extends HTMLElement {
                                           <div id="ld-fancy-heading-634fd7e803d91" class="ld-fancy-heading pos-rel">
                                             <p
                                               class="ld-fh-element d-inline-block pos-rel normal lqd-highlight-classic lqd-highlight-grow-left p">
-                                              290 Maryam Springs 260,<br>
-                                              Courbevoie, Paris, France
+                                              Dumdum Cantonment,<br>
+                                              Kolkata-700065
                                             </p>
                                           </div>
                                         </div>
@@ -1229,30 +1191,19 @@ class Header extends HTMLElement {
                                           </style>
                                           <div class="elementor-social-icons-wrapper elementor-grid">
                                             <span class="elementor-grid-item">
-                                              <a class="elementor-icon elementor-social-icon elementor-social-icon-pinterest-p elementor-repeater-item-23f460f"
-                                                target="_blank">
-                                                <span class="elementor-screen-only">Pinterest-p</span>
-                                                <svg class="e-font-icon-svg e-fab-pinterest-p">
-                                                  <use xlink:href="#fab-pinterest-p">
-                                                  </use>
-                                                </svg> </a>
-                                            </span>
-                                            <span class="elementor-grid-item">
                                               <a class="elementor-icon elementor-social-icon elementor-social-icon-twitter elementor-repeater-item-8d38b73"
-                                                target="_blank">
+                                                target="_blank" href="https://www.facebook.com/profile.php?id=100084401384179&mibextid=ZbWKwL">
                                                 <span class="elementor-screen-only">Twitter</span>
-                                                <svg class="e-font-icon-svg e-fab-twitter">
-                                                  <use xlink:href="#fab-twitter">
-                                                  </use>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                                                 </svg> </a>
                                             </span>
                                             <span class="elementor-grid-item">
                                               <a class="elementor-icon elementor-social-icon elementor-social-icon-instagram elementor-repeater-item-ac72760"
-                                                target="_blank">
+                                                target="_blank" href="https://instagram.com/square_one_interiors?igshid=MzNlNGNkZWQ4Mg==">
                                                 <span class="elementor-screen-only">Instagram</span>
-                                                <svg class="e-font-icon-svg e-fab-instagram">
-                                                  <use xlink:href="#fab-instagram">
-                                                  </use>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                                                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
                                                 </svg> </a>
                                             </span>
                                           </div>
